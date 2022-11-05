d4a4505ae32fca91208eefa346a2aa308e9e90cc
adurga-ariqt <adurga@ariqt.com>
2022-11-05 15:25:48 +0530

    Add project files.

---
+++ Test1_Bhavani_EmployeeDetails/EmployeeDetails.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_Bhavani_EmployeeDetails
+{
+    public  class EmployeeDetails
+    {
+       
+        public double HRA { get; set; }
+        public double TA { get; set; }
+        public double Tax { get; set; }
+        public double DA { get; set; }
+        public double Salary { get; set; }
+        public string EName { get; set; }
+        public int Id { get; set; } 
+        public string Addres { get; set; }
+
+        public double Netsalary { get; set; }
+         public double bonus = 2000;
+
+
+        public void EmployeeSalaryMethod()
+        {
+            double bonus = 2000;
+            if (Salary < 10000)
+            {
+                HRA = Salary * 0.2;
+                TA = Salary * 0.1;
+                Tax = Salary * 0.1;
+                DA = Salary * 0.15;
+            }
+
+            else
+            {
+                HRA = Salary * 0.3;
+                TA = Salary * 0.15;
+                Tax = Salary * 0.2;
+                DA = Salary * 0.2;
+
+            }
+            Netsalary = Salary +bonus+ HRA + TA + DA - Tax;
+            Console.WriteLine("The Netsalary is :" + Netsalary);
+            
+
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("Employee details is");
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("Employee name is " + EName);
+     
+
+            Console.WriteLine("Employee id  is " +Id);
+
+            Console.WriteLine("Employee address is " + Addres);
+
+            Console.WriteLine("Employee salary is " + Salary);
+
+            Console.WriteLine("Employee bonus is " + bonus);
+            Console.WriteLine( "Employee NetSalary is"+Netsalary);
+
+
+
+        }
+
+    }
+}
---
+++ Test1_Bhavani_EmployeeDetails/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test1_Bhavani_EmployeeDetails;
+
+Console.WriteLine("Hello, World!");
+EmployeeDetails obj = new EmployeeDetails();
+obj.EmployeeSalaryMethod();
+Console.WriteLine("Enter employee name:");
+obj.EName = Console.ReadLine();
+Console.WriteLine("Enter employee Id:");
+obj.Id = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter employee Address:");
+obj.Addres = Console.ReadLine();
+Console.WriteLine("Enter employee salary:");
+obj.Salary = Convert.ToDouble(Console.ReadLine());
+obj.EmployeeSalaryMethod();
+obj.DisplayEmployeeDetails();
+Console.ReadLine();
